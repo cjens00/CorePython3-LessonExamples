fd94c9454b740285b3a7e71f6b06aa4c4d3d7edf
Cameron J <@>
2022-08-14 21:51:16 -0600

    Added file I/O section

---
+++ fileio.py
@@ -0,0 +1,9 @@
+
+# import sys
+# sys.getdefaultencoding()
+# e.g. 'utf-8'
+
+# mode: read, write, append : r/w/a
+# combine with selector: t or b, text or binary
+# encoding='utf-8'
+f = open('wasteland.txt', mode='wt')
